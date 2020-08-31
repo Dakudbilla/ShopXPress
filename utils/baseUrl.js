a487737d160e6ccb1b791fcc3ef75284af334873
Akudbilla Daniel <akuddev@gmail.com>
2020-08-31 22:09:00 +0000

    fixed title name

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://shopXPress.now.sh"
+    ? "https://shopx-press.now.sh"
     : "http://localhost:3000";
 
 export default baseUrl;
