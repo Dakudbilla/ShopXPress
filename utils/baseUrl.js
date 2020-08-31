ad7cf01672bd63c1361ad587b348a8ab22739373
Akudbilla Daniel <akuddev@gmail.com>
2020-08-31 23:32:38 +0000

    fixed config error

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://shopx-press.now.sh"
+    ? "https://shopxpress.now.sh"
     : "http://localhost:3000";
 
 export default baseUrl;
